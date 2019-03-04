2a2df73a61cdf39e8555da0cf072a81d2e753174
lesiak <lesiak@outlook.com>
2019-03-04 16:31:24 +0100

    [HELLO_WORLD] Text widget

---
+++ app_0_hello_world/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(buildWidget0());
+
+Widget buildWidget0() {
+  return Center(
+    child: Text(
+      'Hello World',
+      textDirection: TextDirection.ltr,
+    ),
+  );
+}
