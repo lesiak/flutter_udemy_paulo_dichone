fd55bc80f23af0b3b55afb64810526aecd12057f
lesiak <lesiak@outlook.com>
2019-03-06 10:50:42 +0100

    [SCAFFOLD] Scaffold with AppBar

---
+++ app_1_scaffold/lib/ui/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amberAccent,
+        title: Text('Hello World'),
+      ),
+    );
+  }
+}
