9c0a766eb73af8f9a7a45da237d59ba98d12cf66
lesiak <lesiak@outlook.com>
2019-03-06 10:01:24 +0100

    [GESTURES] GestureDetector and SnackBar

--- app_2_gestures/lib/ui/home.dart
+++ app_2_gestures/lib/ui/home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-
   final String title;
 
   const Home({Key key, this.title}) : super(key: key);
@@ -11,6 +10,32 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
+      ),
+      body: Center(
+        child: CustomButton(),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final snackBar = SnackBar(
+          content: Text('Hello Gestures'),
+          backgroundColor: Theme.of(context).backgroundColor,
+        );
+        Scaffold.of(context).showSnackBar(snackBar);
+      },
+      child: Container(
+        padding: EdgeInsets.all(18.0),
+        decoration: BoxDecoration(
+          color: Theme.of(context).buttonColor,
+          borderRadius: BorderRadius.circular(5.5),
+        ),
+        child: Text('First Button'),
       ),
     );
   }
