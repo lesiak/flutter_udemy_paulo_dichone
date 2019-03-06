92040c2adc8e90baa8487fca35174cca4e46295a
lesiak <lesiak@outlook.com>
2019-03-06 11:14:33 +0100

    [STATE] App boilerplate

---
+++ app_3_state/lib/ui/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Make it rain'),),
+      body: Center(
+        child: Text('Hello'),
+      ),
+    );
+  }
+  
+}
