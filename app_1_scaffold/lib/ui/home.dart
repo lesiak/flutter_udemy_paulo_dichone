5bdedd3b06b20873312e05436bac4481b19a7cf7
lesiak <lesiak@outlook.com>
2019-03-06 11:02:13 +0100

    [SCAFFOLD] AppBar actions

--- app_1_scaffold/lib/ui/home.dart
+++ app_1_scaffold/lib/ui/home.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.amberAccent,
         title: Text('Hello World'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.send),
+            onPressed: () => debugPrint('Send icon tapped'),
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: _onSearch,
+          )
+        ],
       ),
     );
+  }
+
+  void _onSearch() {
+    debugPrint('Search icon tapped');
   }
 }
